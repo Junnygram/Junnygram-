244d01a88043a8919bb90566ec5ea98108f78464
Junnygram <you@example.com>
2023-03-01 09:34:55 +0100

    updated my data

--- src/components/data.js
+++ src/components/data.js
@@ -4,20 +4,20 @@ export const data = [
     title: "Frontend Developer intern",
     duration: "1 month currently",
     details:
-      "hbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjb",
+      "So far this has been one of the best intern platform I have been able to pass through, we started by fixing bugs and getting to use chakra-ui. this internship made me versatile and now i can work on a projects that i didnt write the code from scratch",
   },
   {
     year: 2022,
     title: "Frontend Developer intern",
     duration: "6 months",
     details:
-      "hbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjb",
+      "Durng my time in Rivet software i was able to start writing my first react code, we worked on a pet store where animals are being sold and also where you get a fast vetinary support for your pet.",
   },
   {
     year: 2021,
     title: "Mathematice teacher",
     duration: "6 months ",
     details:
-      "hbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjbhbvugvhkbg ivyhbkhvgufyv hvughjb",
+      "During my time working as a teacher i was able to follow schedule and learnt being punctual at work i do. this experience kept me moving forward in my endeavors",
   },
 ];
