5979eee78b6e2dcb72cc1176b57f3a2cf5e30e13
Junnygram <you@example.com>
2023-03-21 12:54:39 +0100

    changed the title

--- src/components/data.js
+++ src/components/data.js
@@ -4,7 +4,7 @@ export const data = [
     title: "Frontend Developer intern",
     duration: "1 month currently",
     details:
-      "So far this has been one of the best intern platform I have been able to pass through, we started by fixing bugs and getting to use chakra-ui. this internship made me versatile and now i can work on a projects that i didnt write the code from scratch",
+      "So far this has been one of the best intern platform I have been able to pass through, we started by fixing bugs and getting to use chakra-ui. this internship made me versatile and now i can work on a projects that i did'nt write the code from scratch",
   },
   {
     year: 2022,
@@ -15,7 +15,7 @@ export const data = [
   },
   {
     year: 2021,
-    title: "Mathematice teacher",
+    title: "Mathematics teacher",
     duration: "6 months ",
     details:
       "During my time working as a teacher i was able to follow schedule and learnt being punctual at work i do. this experience kept me moving forward in my endeavors",
