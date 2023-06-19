f1bc4288d1456da2e6a6ef9df65240cca0372dad
Junnygram <you@example.com>
2023-06-19 10:46:21 +0100

    updating...

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { TypeAnimation } from "react-type-animation";
-import { SocialIcon } from "react-social-icons";
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { SocialIcon } from 'react-social-icons';
 
 const Main = () => {
   let options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   };
   let date = new Date();
-  let formattedDate = date.toLocaleDateString("en-US", options);
+  let formattedDate = date.toLocaleDateString('en-US', options);
 
   return (
     <div id="main" className="">
@@ -24,8 +24,8 @@ const Main = () => {
           <h1 className="sm:text-5xl  text-3xl font-bold text-gray-900">
             <span className="text-xl  ">Hi there, </span>
             <br /> <span className="text-xl">
-              welcome to my portfolio{" "}
-            </span>{" "}
+              welcome to my portfolio{' '}
+            </span>{' '}
             <br />
             I'm Olaleye Oyewunmi
           </h1>
@@ -33,25 +33,25 @@ const Main = () => {
             I'm a
             <TypeAnimation
               sequence={[
-                "Frontend-Developer", // Types 'One'
+                'Frontend-Developer', // Types 'One'
                 2000, // Waits 2s
-                "Coder", // Deletes 'One' and types 'Two'
+                'Coder', // Deletes 'One' and types 'Two'
                 2000, // Waits 2s
-                "Tech Enthusiast", // Types 'Three' without deleting 'Two'
+                'Tech Enthusiast', // Types 'Three' without deleting 'Two'
               ]}
               wrapper="div"
               cursor={true}
               repeat={Infinity}
-              style={{ fontSize: "0.8em" }}
+              style={{ fontSize: '0.8em' }}
             />
           </h1>
           <h1 className="text-[#131518] text-sm">
-            {" "}
+            {' '}
             you visted my portfolio on {formattedDate}
           </h1>
           <div className="flex justify-center gap-4 py-3">
             <SocialIcon
-              url="https://www.linkedin.com/in/olaleye-oyewunmi-4967a5223"
+              url="https://www.linkedin.com/in/olaleyeoyewunmi"
               className="mr-4"
               target="_blank"
               fgColor="#fff"
@@ -66,6 +66,13 @@ const Main = () => {
             />
             <SocialIcon
               url="https://api.whatsapp.com/send/?phone=08055408100&text&type=phone_number&app_absent=0"
+              className="mr-4"
+              target="_blank"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+            <SocialIcon
+              url="https://twitter.com/badboyjunny07"
               className="mr-4"
               target="_blank"
               fgColor="#fff"
