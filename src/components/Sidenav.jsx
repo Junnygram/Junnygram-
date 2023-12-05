8c4190aac462a89ef7c8001edc9147d18c851c02
Junnygram <you@example.com>
2023-12-05 08:37:07 +0100

    added new resume

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -98,7 +98,7 @@ const Sidenav = () => {
               <AiOutlineProject size={20} />
             </a>
             <a
-              href="https://docs.google.com/document/d/1tOOb19BcE2t3wkVM5HHizaUJ-8MFpuoR/edit"
+              href="https://docs.google.com/document/d/1tOOb19BcE2t3wkVM5HHizaUJ-8MFpuoR/edit?usp=sharing&ouid=108904615856114390370&rtpof=true&sd=true"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-100"
             >
               <BsPerson size={20} />
